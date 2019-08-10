3c03566e9f969aa2c3fe4255855bd7e805e76286
robonoriaki <s1526038GQ@s.chibakoudai.jp>
2019-08-10 09:25:16 +0900

    Add a comment to the program that rotates motor

--- dynamixel-rotate_motor.ino
+++ dynamixel-rotate_motor.ino
@@ -2,27 +2,41 @@
 
 DXLIB dxif (true); // select software serial
 void setup() {
-  dxif.begin (80000);
+  dxif.begin (80000); //Baud Rate is 80000
 }
 
 void loop(){
   uint8_t err;
 
+  //CW Angle Limit address is 6, 7
   dxif.WriteWordData (1, 6, 0, NULL);
+  //CCW Angle Limit address is 8, 9
   dxif.WriteWordData (1, 8, 1023, NULL);
   
     //LED ON
+    //LED address is 25
+    //Dynamixel ID is 1
     dxif.WriteByteData (1, 25, 1, &err);
-    //Move to 200
+    
+    //Move to 492
+    //Goal Position address is 30, 31
     dxif.WriteWordData (1, 30, 492, NULL);
+    
+    //Moving Speed address is 32, 33
     dxif.WriteWordData (1, 32, 0, NULL);
+    
     delay(500);
 
     //LED OFF
     dxif.WriteByteData (1, 25, 0, &err);
-    //Move to 400
+    
+    //Move to 532
+    //Goal Position address is 30, 31
     dxif.WriteWordData (1, 30, 532, NULL);
+
+    //Moving Speed address is 32, 33
     dxif.WriteWordData (1, 32, 0, NULL);
+    
     delay(500);
   
 }
