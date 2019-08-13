0546762fefa802217682e496dd2798565f4575c7
robonoriaki <s1526038GQ@s.chibakoudai.jp>
2019-08-13 18:31:30 +0900

    Update comments

--- dynamixel-rotate_motor.ino
+++ dynamixel-rotate_motor.ino
@@ -1,3 +1,5 @@
+/*Motor is rotated alternately CW and CCW direction.*/
+/*The motor rotates alternately in the CW and CCW directions.*/
 #include <dxlib.h>
 
 DXLIB dxif (true); // select software serial
@@ -12,26 +14,27 @@ void loop(){
   dxif.WriteWordData (1, 6, 0, NULL);
   //CCW Angle Limit address is 8, 9
   dxif.WriteWordData (1, 8, 1023, NULL);
-  
-    //LED ON
-    //LED address is 25
+
     //Dynamixel ID is 1
+    //LED address is 25
+    //LED ON -> 1
     dxif.WriteByteData (1, 25, 1, &err);
     
-    //Move to 492
     //Goal Position address is 30, 31
+    //Move to 492
     dxif.WriteWordData (1, 30, 492, NULL);
     
     //Moving Speed address is 32, 33
+    //Max Speed -> 0
     dxif.WriteWordData (1, 32, 0, NULL);
     
     delay(500);
 
-    //LED OFF
+    //LED OFF -> 0
     dxif.WriteByteData (1, 25, 0, &err);
     
-    //Move to 532
     //Goal Position address is 30, 31
+    //Move to 532
     dxif.WriteWordData (1, 30, 532, NULL);
 
     //Moving Speed address is 32, 33
